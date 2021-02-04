b728a3f066caa245d5e297befebb0b37d85b78b8
Rolly <a.mpizarro@alumnos.imf.com>
2021-02-04 11:38:51 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+        <meta name="viewport" content="width=device-width, initial-scale">
+		<link type="text/css" rel="stylesheet" href="./css/estilogame.css">
+
+
+	<title>Game</title>
+</head>
+
+<body{
+  
+}">
+<div class="container">
+	<header><img class="logo" src="./img/logo.png">    </header>
+
+
+	
+
+	<nav> <!-- menuuuu-->
+		<ul class="nav">
+			<li><a href="index.php">Inicio</a></li>
+			<li><a href="#">Productos</a>
+				<ul>
+					<li><a href="#" onclick="alert('estamos en manenimiento sorry')">Ps4</a></li>
+					<li><a href="#" onclick="alert('estamos en manenimiento sorry')">Switch</a></li>
+					<li><a href="#" onclick="alert('estamos en manenimiento sorry')">Xone</a></li>
+					<li><a href="#" onclick="alert('estamos en manenimiento sorry')">Pc Gaming</a>
+						<ul>
+							<li><a href="#">Cascos</a></li>
+							<li><a href="#">Periféricos</a></li>
+							<li><a href="#">Piezas</a></li>
+							<li><a href="#">Accesorios</a></li>			
+						</ul>
+					</li>
+				</ul>
+			</li>
+			<li><a href="# " onclick="alert('estamos en manenimiento sorry')">Seminuevos</a>
+				<ul>
+					<li><a href="#" onclick="alert('estamos en manenimiento sorry')">Ps4</a></li>
+					<li><a href="#" onclick="alert('estamos en manenimiento sorry')">Switch</a></li>
+					<li><a href="#" onclick="alert('estamos en manenimiento sorry')">Xone</a></li>
+					<li><a href="#" onclick="alert('estamos en manenimiento sorry')">Pcgaming</a></li>
+				</ul>
+			</li>
+			<li><a href="#" onclick="alert('estamos en manenimiento sorry')">Black friday</a></li>
+			<li><a href="registro.php">Registro</a></li>
+
+		</ul>
+		</nav>
+
+
+		<div class="informacion"> 
+		En game trabajamos solo para ofrecerle los mejores productos al mejor precio para nuestros clientes, ya que son los que hacen posible que esto siga adelante, a continuación te dejamos las ofertas mas interesantes que hay hasta el momento
+		</div>
+
+
+    <div class="slideshow-container">
+
+        <div class="mySlides fade">
+            <div class="numbertext">1 / 4</div>
+            <img src="./img/1imagenslider.PNG" style="width:100%">
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext">2 / 4</div>
+            <img src="./img/imagen2slider.PNG" style="width:100%">
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext">3 / 4</div>
+            <img src="./img/imagen3slider.PNG" style="width:100%">
+        </div>
+        <div class="mySlides fade">
+            <div class="numbertext">4 / 4</div>
+            <img src="./img/imagen4slider.PNG" style="width:100%">
+        </div>
+
+    </div>
+    <br>
+
+    <div style="text-align:center">
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+
+    </div>
+
+
+    <!-- ANTONIO HE DEJADO AQUI MI JAVASCRIPT PORQUE AL ENLAZARLO NO ME LO COGE, PERDON-->
+
+
+
+    
+
+
+
+
+    <section>
+
+
+
+
+        <a href="reddead.php">
+        <img class="black" src="./img/blackfriday.PNG">
+        </a>
+
+
+	<div class="fotos">
+        <a href="pokemonespada.php">
+            <img class="productos" src="./img/pokemonespada.png">
+        </a>
+        <a href="reddead.php">
+        <img class="productos" src="./img/reddead.png">
+        </a>
+        <a href="kh3.php">
+            <img class="productos" src="./img/kheart3.png">
+        </a>
+
+            <img class="productos" src="./img/sillagaming.png">
+            <img class="productos" src="./img/gow.png">
+            <img class="productos" src="./img/portatil.png">
+            <img class="productos" src="./img/spiderman.png">
+            <img class="productos" src="./img/teclado.png">
+	</div>
+
+<footer>
+
+
+<img class="imgfooter" src="./img/footer.PNG">
+
+</footer>
+
+    </div>
+</body>
+</html>
---
+++ kh3.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html>
+<head>
+
+    <link type="text/css" rel="stylesheet" href="./css/estiloproductos.css">
+
+
+    <title>Kindom Heart 3</title>
+</head>
+<body>
+<div class="container">
+    <header><img class="logo" src="./img/logo.png">    </header>
+
+
+
+
+    <nav> <!-- menuuuu-->
+
+        <ul class="nav">
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="#">Productos</a>
+                <ul>
+                    <li><a href="#" onclick="alert('estamos en manenimiento sorry')">Ps4</a></li>
+                    <li><a href="#" onclick="alert('estamos en manenimiento sorry')">Switch</a></li>
+                    <li><a href="#" onclick="alert('estamos en manenimiento sorry')">Xone</a></li>
+                    <li><a href="#" onclick="alert('estamos en manenimiento sorry')">Pc Gaming</a>
+                        <ul>
+                            <li><a href="#">Cascos</a></li>
+                            <li><a href="#">Periféricos</a></li>
+                            <li><a href="#">Piezas</a></li>
+                            <li><a href="#">Accesorios</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <li><a href="# " onclick="alert('estamos en manenimiento sorry')">Seminuevos</a>
+                <ul>
+                    <li><a href="#">Ps4</a></li>
+                    <li><a href="#">Switch</a></li>
+                    <li><a href="#">Xone</a></li>
+                    <li><a href="#">Pcgaming</a></li>
+                </ul>
+            </li>
+            <li><a href="#" onclick="alert('estamos en manenimiento sorry')">Black friday</a></li>
+            <li><a href="registro.php">Registro</a></li>
+
+        </ul>
+    </nav>
+
+    <section>
+        <div class="imgs">
+            <img class="foto" src="./img/kheartprecio.PNG">
+            <img class="foto" src="./img/kheart32.PNG">
+            <img class="info" src="./img/kheart3info.PNG">
+        </div>
+
+    </section>
+
+
+    <footer>
+
+        <img class="imgfooter" src="./img/footer.PNG">
+
+    </footer>
+
+</div>
+</body>
+</html>
+
+</html>
